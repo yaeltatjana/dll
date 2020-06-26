92bf28d5b4b4636db2d539a9a864785dd8c95aae
Yael <yael.iseli@edu.hefr.ch>
2020-06-26 20:21:50 +0200

    Remove useless method

--- python-wrapper-lib/DenseDenseNet.cpp
+++ python-wrapper-lib/DenseDenseNet.cpp
@@ -48,32 +48,3 @@ void DenseDenseNet::storeWeights(const std::string& file) {
 void DenseDenseNet::loadWeights(const std::string& file) {
     net->load(file);
 }
-
-void DenseDenseNet::all() {
-    // initialization
-    using dbn =
-    dll::dbn_desc<
-            dll::dbn_layers < dll::dyn_dense_layer_desc < dll::activation < dll::function::RELU>>::layer_t,
-            dll::dyn_dense_layer_desc < dll::activation < dll::function::SOFTMAX >> ::layer_t >,
-            dll::updater < dll::updater_type::MOMENTUM >,
-            dll::trainer < dll::sgd_trainer >,
-            dll::shuffle,
-            dll::batch_size < 100 >> ::dbn_t;
-
-    auto n = std::make_unique<dbn>();
-    auto ds = dll::make_mnist_dataset(dll::batch_size < 100 > {}, dll::scale_pre < 255 > {});
-
-    n->template layer_get<0>().init_layer(28 * 28, 28 * 28);
-    n->template layer_get<1>().init_layer(28 * 28, 32);
-    n->learning_rate = 0.001;
-
-    // display infos
-    ds.display();
-    n->display();
-
-    // train
-    n->fine_tune(ds.train(), 5);
-
-    // evaluate
-    n->evaluate(ds.test());
-}
